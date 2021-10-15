3e40d1848e7d6d531fc0793e2645ff6d83aa23fa
lexynotfound <36406297+lexynotfound@users.noreply.github.com>
2021-10-16 01:13:01 +0700

    Add files via upload

---
+++ demonstrasi.py
@@ -0,0 +1,40 @@
+import math
+
+    ## operator aritmatika jumlah
+def demonstrasi_perjumlahan():
+    angka1=90
+    angka2=20
+
+    print("Perjumlahan       \t:", angka1+angka2)
+    print("Akar dari         \t:", math.pow(3,3))
+
+
+    ## operator aritmatika pengurangan
+def demonstrasi_pengurangan():
+    angka1=90
+    angka2=20
+
+    print("Pengurangan       \t:", angka1-angka2)
+    print("Akar dari         \t:", math.pow(5,5))
+
+
+    ## operator aritmatika pembagian
+def demonstrasi_pembagian():
+    angka1=100
+    angka2=100
+
+    print("Pembagian       \t:", angka1//angka2)
+    print("Akar dari       \t:", math.pow(4,4))
+
+    ## operator aritmatika peerkalian
+def demonstrasi_perkalian():
+    angka1=80
+    angka2=100
+
+    print("Perkalian       \t:",angka1*angka2)
+    print("Akar dari       \t: ", math.pow(10,10))
+
+demonstrasi_perjumlahan()
+demonstrasi_pengurangan()
+demonstrasi_pembagian()
+demonstrasi_perkalian()
