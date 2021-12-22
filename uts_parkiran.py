03cfc06a03ea5c09391adb4919f9af893f076543
lexynotfound <36406297+lexynotfound@users.noreply.github.com>
2021-12-23 00:07:14 +0700

    Add files via upload

--- uts_parkiran.py
+++ uts_parkiran.py
@@ -1,59 +1,59 @@
-## UTS MEMBUAT APLIKAI PARKIR##
-## NAMA : KURNIA RAIHAN ARDIAN##
-## NIM : 2122.01.0018##
-## PRODI : SISTEM INFORMASI##
-## KAMPUS : IDS##
-
-
 import datetime as dt
 import math
+from datetime import datetime
+import qrcode
 
 ## Display atau menampilkan nama
-def display_intro():
-    title = "** Selamat datang di Parkiran AR **"
+
+## menentukan tipe kendaraan
+def tipe_kendaraan():
+    title = "* Selamat datang di Parkiran AR *"
     print("*" * len(title))
     print(title)
     print("*" * len(title))
 
-## menentukan tipe kendaraan
-def tipe_kendaraan():
     tipe_kendaraan = str(input("Pilih jenis kendaraaan anda : \n \t \n 1. Mobil \t \n 2. Motor \n\nMasukkan tipe kendaraan anda :"))
-    print("***********************************")
-    jam_masuk = int(input(" Masukkan Jam Masuk \t: "))
-    jam_keluar = int(input(" Masukkan Jam Keluar \t: "))
-    
+    print("*****")
+    jam_masuk = str(datetime.now())[11:19]
+    jam_keluar = str(datetime.now())[11:19]
+
 
     if tipe_kendaraan == "1" or tipe_kendaraan == "Mobil, MOBIL, mobil":
-        lama_parkir = jam_keluar - jam_masuk
+        lama_parkir = jam_masuk
         biaya_parkir = 10000
 
-        if (biaya_parkir >= lama_parkir):
-            biaya_parkir = +5000-5000+(biaya_parkir * lama_parkir + 5000)
-            print("***********************************")
-            print("Lama Parkir Mobil   : ", lama_parkir, "jam\n")
-            print("Biaya Parkir Mobil  :  Rp.", biaya_parkir)
-            print("***********************************")
-            print("***********************************")
-            print("Lama Parkir Mobil   : ", lama_parkir, "jam\n")
-            print("Biaya Parkir Mobil  :  Rp.", biaya_parkir)
-            print("***********************************")
+        #biaya_parkir = +5000-5000+(biaya_parkir * lama_parkir + 5000)
+        print("*****")
+        print("Mulai Parkir Mobil Pukul :", lama_parkir )
+        print("Biaya Parkir Mobil  :  Rp.", biaya_parkir)
+        print("*****")
 
     elif tipe_kendaraan == "2" or tipe_kendaraan == "Motor, motor, MOTOR":
-        lama_parkir = jam_keluar - jam_masuk
+        lama_parkir = jam_masuk
         biaya_parkir_motor = 5000
 
-        if (biaya_parkir_motor >= lama_parkir):
-            biaya_parkir_motor = (biaya_parkir_motor * lama_parkir)
+        print("*****")
+        print("Mulai Parkir Motor Pukul :", lama_parkir)
+        print("Biaya Parkir Motor  :  Rp.", biaya_parkir_motor)
+        print("*****")
 
-            print("***********************************")
-            print("Lama Parkir Motor   : ", lama_parkir, "jam\n")
-            print("Biaya Parkir Motor  :  Rp.", biaya_parkir_motor)
-            print("***********************************")
-            print("***********************************")
-            print("Lama Parkir Motor   : ", lama_parkir, "jam\n")
-            print("Biaya Parkir Motor  :  Rp.", biaya_parkir_motor)
-            print("***********************************")
+def tipe_qrcode():
+    biaya_parkir = 10000
+    jam_masuk = str(datetime.now())[11:19]
+    
+    qr = qrcode.QRCode(
+        version=5,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=15,
+        border=5
+    )
+    data = "Jam Masuk : ", jam_masuk, " Biaya Parkir : ", biaya_parkir
+    qr.add_data(data)
+    qr.make()
+    img = qr.make_image(fill='black', back_color='white')
+    type(img)
+    img.save('Struk Parkir.png')
+    
 
-
-display_intro()
 tipe_kendaraan()
+tipe_qrcode()
